3ba34b9e076e02ce2f0f5e6c07feaa6405d5dd25
Raphael Tan <raphaeltanyw@gmail.com>
2016-01-18 21:24:03 +0800

    Add exception handling

--- bot.rb
+++ bot.rb
@@ -7,25 +7,37 @@ $wikia_page = ENV["WIKIA_PAGE"]
 $bot_name = ENV["BOT_NAME"]
 
 def get_query_results(search_string)
-  url = URI.escape("http://#{$wikia_page}.wikia.com/api/v1/Search/List/?query=#{search_string}&limit=10")
-  response = HTTParty.get(url)
-  response.parsed_response["items"]
+  begin
+    url = URI.escape("http://#{$wikia_page}.wikia.com/api/v1/Search/List/?query=#{search_string}&limit=10")
+    response = HTTParty.get(url)
+    response.parsed_response["items"]
+  rescue Exception => e
+    []
+  end
 end
 
 def get_article_abstract_thumbnail(id)
-  response = HTTParty.get("http://#{$wikia_page}.wikia.com/api/v1/Articles/Details/?ids=#{id}&abstract=100")
-  item = response.parsed_response["items"]["#{id}"]
-  return item['abstract'], item['thumbnail']
+  begin
+    response = HTTParty.get("http://#{$wikia_page}.wikia.com/api/v1/Articles/Details/?ids=#{id}&abstract=100")
+    item = response.parsed_response["items"]["#{id}"]
+    return item['abstract'], item['thumbnail']
+  rescue Exception => e
+    return nil, nil
+  end
 end
 
 def create_inline_response(results_list)
   return [] if results_list.nil?
-  results_list.map do |result|
-    abstract, thumbnail = get_article_abstract_thumbnail(result["id"])
-    Telegram::Bot::Types::InlineQueryResultArticle.new(
-      id: result["id"], title: result["title"], url: result["url"],
-      message_text: result["url"], description: abstract, thumb_url: thumbnail
-    )
+  begin
+    results_list.each_with_index.map do |result, i|
+      abstract, thumbnail = get_article_abstract_thumbnail(result["id"])
+      Telegram::Bot::Types::InlineQueryResultArticle.new(
+        id: i, title: result["title"], url: result["url"],
+        message_text: result["url"], description: abstract, thumb_url: thumbnail
+      )
+    end
+  rescue Exception => e
+    []
   end
 end
 
